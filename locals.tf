8a1eecd6c41e0f3066c250c63ef3cc9757bebc20
Jarosław Leśniak <jarek@Jarosaws-MacBook-Pro.local>
2024-02-12 22:00:46 +0100

    Sync GitHub

--- locals.tf
+++ locals.tf
@@ -14,7 +14,7 @@ locals {
 
     "fem-eci-github" = {
       description = "Some github description"
-      execution_mode = "local"
+      execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
