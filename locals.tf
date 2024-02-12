6154799ba6b8c2cadc7cc54a4c62947bfc6ebe43
Jarosław Leśniak <jarek@Jarosaws-MacBook-Pro.local>
2024-02-12 20:16:58 +0100

    Setup github workspace

--- locals.tf
+++ locals.tf
@@ -9,7 +9,14 @@ locals {
       description = "Some workspace description"
       execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "jaroslawlesniak/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+
+    "fem-eci-github" = {
+      description = "Some github description"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
